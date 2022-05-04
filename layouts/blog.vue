fad624d4bfa763feb2af02996fdc259ffb5c0dbd
Gordon <gopike@gmail.com>
2022-05-04 17:10:45 -0600

    Intial

---
+++ layouts/blog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-6 gap-4">
+    <div class="col-span-6"><header-component></header-component></div>
+    <div class="col-span-4"><Nuxt /></div>
+    <div class="col-span-2"><side-component></side-component></div>
+    <div class="col-span-6"><footer-component></footer-component></div>
+  </div>
+</template>
+<script>
+import HeaderComponent from '../components/Header.vue'
+import FooterComponent from '../components/Footer.vue'
+import SideComponent from '../components/Side.vue'
+export default {
+  components: {
+    'header-component': HeaderComponent,
+    'footer-component': FooterComponent,
+    'side-component': SideComponent,
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="">Hello</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  layout: 'blog',
+})
+</script>
