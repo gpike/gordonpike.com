d049ccca31bf4c807c7491b99c54a702e9003ced
Gordon <gopike@gmail.com>
2022-05-09 19:02:21 -0600

    feat: added seo fields to main page

--- content/index.md
+++ content/index.md
@@ -5,13 +5,15 @@ published: true
 description: 'Description of page'
 is_canonical: true
 canonical_url: ''
-meta_description: ''
-og_title: ''
-og_type: ''
-og_url: ''
-og_description: ''
+meta_description: This is the great index page
+og_title: Blog Home Page
+og_type: page
+og_url: url
+og_description: This is the great index page
 og_image: ''
-keywords: []
+keywords:
+  - blog
+  - engine
 ---
 
 Page description here
--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,59 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'IndexPage',
   layout: 'blog',
+  async asyncData({ $content }) {
+    const pageConfig = await $content('index').fetch()
+
+    return {
+      pageConfig,
+    }
+  },
+  data() {
+    return {
+      absUrl: '',
+    }
+  },
+  head() {
+    return {
+      title: (this as any).pageConfig.title,
+      meta: [
+        {
+          hid: 'description',
+          property: 'description',
+          content: (this as any).pageConfig.description,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: (this as any).pageConfig.og_title,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: (this as any).pageConfig.og_description,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: (this as any).pageConfig.og_image,
+        },
+        {
+          hid: 'keywords',
+          property: 'keywords',
+          content: (this as any).pageConfig.keywords,
+        },
+      ],
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: (this as any).absUrl,
+        },
+      ],
+    }
+  },
+  beforeMount() {
+    this.absUrl = `${window.location.origin}${this.$route.fullPath}`
+  },
 })
 </script>
